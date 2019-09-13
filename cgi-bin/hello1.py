ffeea3ccf4a22acc184b16caec2118fd728bbbda
Nishant.Nikhil <nishant.nikhil@NishantNikhils-MacBook-Pro.local>
2019-09-13 17:01:06 +0200

    hello1.pyadded

---
+++ cgi-bin/hello1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session, flash, send_file, send_from_directory
+from werkzeug import secure_filename
+import collections
+import MySQLdb
+from crypt import methods
+import os
+from flask.globals import request
+import logging
+import yaml
+from datetime import date, timedelta
+#added comment
+app =Flask(__name__)
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
